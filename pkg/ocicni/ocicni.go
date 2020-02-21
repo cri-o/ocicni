c0cef6a42b97717b2461108f0f142a783403351d
Chan Shih-Ping <shihping.chan@gmail.com>
2020-02-22 00:37:12 +0800

    Allow = inside environment variable body
    Signed-off-by: Chan Shih-Ping <shihping.chan@gmail.com>

--- pkg/ocicni/ocicni.go
+++ pkg/ocicni/ocicni.go
@@ -811,7 +811,7 @@ func buildCNIRuntimeConf(cacheDir string, podNetwork *PodNetwork, ifName string,
 
 	// Propagate existing CNI_ARGS to non-k8s consumers
 	for _, kvpairs := range strings.Split(os.Getenv("CNI_ARGS"), ";"){
-		if keyval := strings.Split(kvpairs, "="); len(keyval) == 2 {
+		if keyval := strings.SplitN(kvpairs, "=", 2); len(keyval) == 2 {
 			rt.Args = append(rt.Args, [2]string{keyval[0], keyval[1]})
 		}
 	}
